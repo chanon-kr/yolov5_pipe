05d8fa79aa8846cd145765c00a03a76bf4e8f0bb
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-01-12 11:12:43 +0700

    Correct localframework called folder

--- F00_script/func_utilities.py
+++ F00_script/func_utilities.py
@@ -57,8 +57,8 @@ def setup_model(model_name, force_reload, device_type, conf, iou, class_detect, 
         os.chdir(base_dir)
         try : t_update_local_framework()
         except : print('Update Fail')
-        model = torch.hub.load(os.path.join('ultralytics','yolov5'), 'custom', path = model_name
-                                , source ='local'
+        model = torch.hub.load(os.path.join(base_dir,'ultralytics','yolov5'), 'custom'
+                                , path = model_name, source ='local'
                                 , force_reload = force_reload, device = device_type) 
     else :
         model = torch.hub.load('ultralytics/yolov5' , 'custom', path = model_name
