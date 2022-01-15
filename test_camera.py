7261d50a94a5d57daab6cbec85294130274251f8
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-01-15 10:15:27 +0700

    Add Test Camera File

---
+++ test_camera.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import cv2
+
+# Read Input File
+input_df = pd.read_excel('F01_config/config.xlsx', sheet_name = 'config'
+                        , dtype = str, index_col='parameter')[['value']].fillna('')
+input_df = input_df[input_df['value'] != '']['value'].to_dict()
+
+# Get Video Source
+video_source = input_df.get('video_source','0')
+if video_source.replace('.','',1).isdigit() : video_source = int(video_source)
+
+# Read Video
+video = cv2.VideoCapture(video_source)
+
+while(video.isOpened()):
+    # Check
+    ret, frame = video.read()
+    if not ret:
+        print('Reached the end of the video!')
+        break
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'): break
+
+# Clean Up
+video.release()
+cv2.destroyAllWindows()
