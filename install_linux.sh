a6d2ac6b7d0a8b8c86be50b645c20fc206418d27
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-01-25 13:21:52 +0700

    Add pytorch install script

--- install_linux.sh
+++ install_linux.sh
@@ -14,3 +14,9 @@ sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
 sudo apt-get -y install qt4-dev-tools libatlas-base-dev
 pip3 install opencv-python
+
+# Pytorch
+wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
+pip3 install Cython
+pip3 install numpy torch-1.8.0-cp36-cp36m-linux_aarch64.whl
