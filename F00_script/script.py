908a3f4846e701a5950b031d500f0f394eddb368
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-03-24 08:49:28 +0700

    Relocate show temp video

--- F00_script/script.py
+++ F00_script/script.py
@@ -199,14 +199,14 @@ def main_script() :
                     temp_begin = datetime.now()
                     temp_video = cv2.VideoWriter(temp_video_name, cv2.VideoWriter_fourcc(*'DIVX')  , temp_fps, (int(imW), int(imH)))
                     prev = datetime.now()
-                # Limit FPS
-                if (now - prev).total_seconds() < temp_min_time : continue
                 # Show
                 if display_output :
                     if resize_show :
                         resize_image = cv2.resize(frame, (resize_width_show, resize_height_show))
                         cv2.imshow('Object detector', resize_image)
                     else : cv2.imshow('Object detector', frame)      
+                # Limit FPS
+                if (now - prev).total_seconds() < temp_min_time : continue
                 # Check Length Video          
                 if (datetime.now() - temp_begin).total_seconds() < temp_length :
                     temp_video.write(frame)
