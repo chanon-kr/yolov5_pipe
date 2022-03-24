e4d07e7a50d7d2e2b6e6947f1bedc34b84b314f3
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-03-24 10:20:12 +0700

    test display

--- F00_script/script.py
+++ F00_script/script.py
@@ -205,19 +205,18 @@ def main_script() :
                 if (datetime.now() - temp_begin).total_seconds() < temp_length :
                     temp_video.write(frame)
                     prev = datetime.now()
-                    # Show
-                    if display_output :
-                    #     if resize_show :
-                        # resize_image = cv2.resize(frame, (resize_width_show, resize_height_show))
-                        # cv2.imshow('Object detector', resize_image)
-                    #     else : 
-                        cv2.imshow('Object detector', frame)      
                 else : 
                     temp_video.release()
                     temp_video,record_temp = None, False
                     video.release()
                     print('Read temp video')
                     video = cv2.VideoCapture(temp_video_name)
+                # Show
+                if display_output :
+                    if resize_show :
+                        resize_image = cv2.resize(frame, (resize_width_show, resize_height_show))
+                        cv2.imshow('Object detector', resize_image)
+                    else : cv2.imshow('Object detector', frame)      
                 continue
 
             # Inference
